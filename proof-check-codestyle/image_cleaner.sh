9c1b80fb939f4cc37f56aad54129b56d9a8aa725
Denis Kormalev <denis.kormalev@opensoftdev.com>
2018-07-24 13:23:18 -0700

    redone images tree

---
+++ proof-check-codestyle/image_cleaner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf /var/lib/apt/lists/*
+find /usr/share/locale -name '??' -o -name '??_??' | xargs rm -rf
+find /usr/share/doc -type f -a ! -iname 'copyright*' -delete
+rm -rf /var/tmp/*
+rm -rf /tmp/*
+rm -rf /usr/share/man
