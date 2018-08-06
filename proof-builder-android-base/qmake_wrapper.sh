fc74046b423190c8156454059f2c7fff9d9930d4
Denis Kormalev <denis.kormalev@opensoftdev.com>
2018-08-06 14:39:16 -0700

    Qt 5.10.1 instead of 5.11.1 in android-base due to strange crashes with 5.11.1

--- proof-builder-android-base/qmake_wrapper.sh
+++ proof-builder-android-base/qmake_wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo '$' qmake -r -spec android-g++ "QMAKE_CC=$CC" "QMAKE_CXX=$CXX" "QMAKE_LINK=$CXX" "$@"
+echo '$' qmake -r -spec android-g++ \"QMAKE_CC=$CC\" \"QMAKE_CXX=$CXX\" \"QMAKE_LINK=$CXX\" "$@"
 qmake -r -spec android-g++ "QMAKE_CC=$CC" "QMAKE_CXX=$CXX" "QMAKE_LINK=$CXX" "$@"
